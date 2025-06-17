3f35a7fc32ea230baac8815bacc69fbd8bc8510b
Dilraj <dilraj.nagapure@gmail.com>
2025-06-17 21:15:41 +0530

    added segger settings, not working

---
+++ 001_TaskCreation/Debug/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.d
@@ -0,0 +1,36 @@
+ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.o: \
+ ../ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.c \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/SEGGER/SEGGER.h \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/Config/Global.h \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/include/task.h \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/include/list.h \
+ ../ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/SEGGER/SEGGER.h:
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/Config/Global.h:
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/include/portable.h:
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/include/task.h:
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/FreeRTOS/include/list.h:
+../ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
---
+++ 001_TaskCreation/Debug/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
@@ -0,0 +1,6 @@
+ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
+ ../ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+C:/Work/FreeRTOS_STM32/001_TaskCreation/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
